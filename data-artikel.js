81f1e216cab7dec90c97626f7d31ec8c1738a3ca
jakarta-utara <akubodong@outlook.co.id>
2026-01-08 15:03:06 +0800

    Create data-artikel.js

---
+++ data-artikel.js
@@ -0,0 +1,26 @@
+const daftarArtikel = [
+    {
+        id: "cincin-berlin-jakarta-utara", // ID ini harus unik
+        tag: "resep",
+        judul: "cincin berlian tersedia di daerah jakarta utara",
+        cuplikan: "Cara beli..",
+        gambar: "https://pbs.twimg.com/media/DS_sBYSV4AAdcEC.jpg",
+        tanggal: "2026-01-08",
+        isiLengkap: `
+            <h3>Langkah pesanan</h3>
+            <p>Siapkan susu cair dan bubuk agar-agar, aduk hingga mendidih.</p>
+            <div class="video-container">
+                <iframe src="https://www.youtube.com/embed/pchKoaPHBM_qjFIW"></iframe>
+            </div>
+        `
+    },
+    {
+        id: "berita-harga-cincin-berlian",
+        tag: "berita",
+        judul: "Update Harga berlian Hari Ini",
+        cuplikan: "Laporan harian mengenai harga berlian cepat...",
+        gambar: "https://pbs.twimg.com/media/GEeaLY7XsAAvDQR.jpg",
+        tanggal: "2026-01-08",
+        isiLengkap: `<p>Harga berlian...</p>`
+    }
+];
